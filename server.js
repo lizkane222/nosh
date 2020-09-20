c134b02fba79cf4fa0d78e14bbf3f76be8da87f4
Liz Kane <lizkane222@gmail.com>
2020-09-19 17:08:21 -0700

    Core File Structure is Officially Ready

---
+++ server.js
@@ -0,0 +1,24 @@
+/* External Modules */
+
+/* Internal Modules */
+
+/* Instanced Modules */
+
+/* Configuration */
+
+/* middleware */
+
+/* Routes */
+
+
+
+// view routes
+
+// recipe Routes
+
+// user Routes
+
+/* Server Listener */
+
+
+
