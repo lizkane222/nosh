eb387a8dc3d6d9271c30a1a630cae0162a35e064
Liz Kane <lizkane222@gmail.com>
2020-09-22 13:03:29 -0700

    edit route it connected

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -15,17 +15,7 @@ const db = require('../models');
 router.get('/new', (req,res) => {
     res.render('recipe/new.ejs');
 });
-// router.get('/newForm', (req,res) => {
-    // db.Recipe.find({}, (err, foundRecipe) => {
-    //     if(eff) return res.send(err);
 
-    //     const context = {
-    //         recipes : foundRecipe,
-    //     }
-
-    //     res.render('recipe/new.ejs', context);
-    // })
-// });
 
 // create recipe  /recipe     //new.ejs
 router.post('/new', (req,res) => {
@@ -39,8 +29,6 @@ router.post('/new', (req,res) => {
         }
     })
 });
-
-
 
 
 // show ONLY ONE recipe  /recipe      //show.ejs
@@ -59,33 +47,32 @@ router.get('/:id', (req,res) => {
 // edit recipe  <- view   /recipe      //edit.ejs
 // router.get
 router.get("/:id/edit", (req, res) => {
+    // res.send('THE RECIPE IS READY TO EDIT')
     db.Recipe.findById(req.params.id, (err, foundRecipe) => {
       if (err) {
         console.log(err);
         return res.send(err);
       }
       const context = { recipe: foundRecipe };
-      res.render("article/edit", context);
+        res.render(`recipe/edit`, context);
     });
-  });
+});
 
 
 
 // update <- db change   /recipe       //edit.ejs & index.ejs
-router.put("/:id", (req, res) => {
-    db.Recipe.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true },
-      (err, updatedRecipe) => {
-        if (err) {
-          console.log(err);
-          return res.send(err);
-        }
-        res.redirect(`/recipe/${updatedRecipe._id}`);
-      }
-    );
-  });
+// router.put("/:id", (req, res) => {
+//     db.Recipe.findByIdAndUpdate( req.params.id,req.body,{ new: true },(err, updatedRecipe) => {
+//         if (err) {
+//           console.log(err);
+//           return res.send(err);
+//         }
+//         const context = { recipe: updatedRecipe };
+        
+//         // res.redirect(`/recipe/${updatedRecipe._id}`, context);
+//       }
+//     );
+//   });
 
 // delete   N/A
 
