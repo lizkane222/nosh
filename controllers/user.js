bc39db69af938fdd92fb4f6bb0b7ad2985a65b9c
jrandersen-ga <andersen.ja@gmail.com>
2020-09-23 15:20:12 -0400

    fixed bug from laste save, back to same place

--- controllers/user.js
+++ controllers/user.js
@@ -22,32 +22,24 @@ const loginReqired = function(req, res, next) {
 /* ===== USER PANTRY ROUTES ===== */
 
 // GET (user) index
-router.get("/", loginReqired,  async (req, res) => {
-    try {
-        // const foundUser = await db.User.find({});
-        const foundUser = db.User.findById(req.session.currentUser.id)
-        // console.log(foundUser);
-        const context = { user: foundUser }
-        // console.log(context);
-        res.render('user/index', context);
-    } catch (error) {
-        console.log(error);
-        res.send({ message: "Internal Server Error" });
-    }
+router.get("/", loginReqired,  (req, res) => {
+    db.User.find({}, (error, allUsers) => {
+      if (error) return res.send(error);
+      const context = { 
+        users: allUsers 
+      };
+      res.render("/user/index", context);
+    });
 });
 
-// POST  === NO LONGER NEEDED ===  NEW USER (WITH NO AUTH) 
-// router.post("/", (req, res) => {
-//     db.User.create(req.body, (err, createdUser) => {
-//         console.log(createdUser);
-//       if (error) {
-//         console.log(error);
-//         return res.send(error);
-//       }
-//       res.redirect("/users");
-//     });
-// });
+// switch
 
+// post recipe to user nosh 
+router.get("/nosh", (req, res) => {
+  res.send('Hello noshers!');
+});
+
+//(when items in nosh for user, show (unsave) on save button in index & show page)
 // GET (show) USER PANTRY FROM NAVBAR
 router.get("/pantry", loginReqired, (req, res) => {
   db.User.findById(req.session.currentUser.id, (error, foundUser) => {
@@ -109,7 +101,7 @@ router.get("/:id/edit", loginReqired, (req, res) => {
 
 // GET (edit) FOODITEM UPDATE FORM PANTRY
 router.get("/:id/editItem", loginReqired, (req, res) => {
-  // res.send("fooditem ping back!"
+  // res.send("fooditem ping back!)"
   db.User.findById(req.session.currentUser.id,  (err, foundUser) => {
     if (err) {
     console.log(error)
@@ -122,45 +114,62 @@ router.get("/:id/editItem", loginReqired, (req, res) => {
   });
 });  
 
+// PUT (updateItem) FOORITEM FROM UPDATEITEM FORM TO DB AND BACK TO USER
 router.put("/:id/updateItem", loginReqired, async (req, res) => {
-  // res.send(' hello I PUT route for item ')
     try {
-      // const updatedItem = {
-      //   $push: {pantry: {
-      //           foodItem: req.body.foodItem,
-      //           quantity: req.body.quantity,
-      //           unit: req.body.unit,
-      //   }},
-      // };
-      const newItem = {foodItem: req.body.foodItem, quantity: req.body.quantity, unit: req.body.unit,}
-      console.log(` newitem ${newItem}`);
-
+      // get the user from db
       const foundUser = await db.User.findById(req.session.currentUser.id)
-      console.log(` params id ${req.params.id}`)
       
-     let item = foundUser.pantry.id(req.params.id)
-      console.log(item)
-
-      // item = {...item,...newItem};
-      // foundUser.pantry.splice(item, newItem);
+      // get the user.pantry{foodites,qty,unit}
+      let item = foundUser.pantry.id(req.params.id)
+      
+      // update items from db (left) to items from form (right)
+      item.foodItem = req.body.foodItem;
+      item.quantity = req.body.quantity;
+      item.unit = req.body.unit;
+      
+      // just await then save the user
       await foundUser.save();
 
+      // construct user context to send back to page
       const context = { user: foundUser }
       res.render(`user/show`, context);
     } catch (error) {
       console.log(error);
-      res.send( {message: "Something went horribly wrong [in your PUT Item route] please go back... in time"} );
+      res.send( {message: "Something went horribly wrong [in your PUT Food Item route] please go back... in time"} );
     }
 });
+
+// DELETE (PANTRY ITEM)
+router.delete("/:id/updateItem", loginReqired, async (req, res) => {
+  try {
+    // get the user from db
+    const foundUser = await db.User.findById(req.session.currentUser.id)
+
+    // get the user.pantry{foodites,qty,unit} and just dot remove & then save
+    let item = foundUser.pantry.id(req.params.id);
+    item.remove();
+    await foundUser.save();
+
+    // construct user context to send back to page
+    const context = { user: foundUser }
+    res.render(`user/show`, context);
+  } catch (error) {
+    console.log(error);
+    res.send( {message: "Something went horribly wrong [in your DELETE FOOD Item route] please go back... in time"} );
+  }
+}); 
+
+
 // DELETE (user) (no auth) 
 // TODO (this will need ot look thoough recipies too) similar to autHors and articles example
-router.delete("/:id", function (req, res) {
+router.delete("/:id", (req, res) => {
     db.User.findByIdAndDelete(req.params.id, (error, deletedUser) => {
       if (error) {
         console.log(err);
         return res.send(err);
       }
-      console.log(deletedUser);
+      // console.log(deletedUser);
       res.redirect("/users");
     });
 });
@@ -171,10 +180,9 @@ router.delete("/:id", function (req, res) {
 /* TODO ===== do third ===== */
 /* ===== USER NOSH ROUTES ===== */
 // get index 
-        //once user selects (save) it executes post below
+//once user selects (save) it executes post below === NO GET ROUTE IN THIS CASE ===
     
-// post recipe to user nosh 
-        //(when items in nosh for user, show (unsave) on save button in index & show page)
+
 // get index
         //(if in nosh, the delete form nsoh and show button as (save))
 // delete recipe from nosh
