6253b4926e9b8a841a097caa4106fac79fa2122f
jrandersen-ga <andersen.ja@gmail.com>
2020-09-28 22:06:37 -0400

    adds functionality of edit recipe/food items

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { isValidObjectId } = require('mongoose');
 const router = express.Router();
 const db = require('../models');
 
@@ -87,6 +88,19 @@ router.get("/:id/edit", (req, res) => {
     });
 });
 
+ //PUT recipe:id upadte recipe
+ router.put("/:id/updateRecipe", loginRequired, async (req, res) => {
+    try {
+      // get the recipe from db
+      const foundRecipe = await db.Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      const context = { recipe: foundRecipe, user: req.session.currentUser };
+      res.redirect('/')
+    } catch (error) {
+    console.log(error);
+    res.send( {message: "Something went horribly wrong [in your PUT UpdateRecipe route] please go back... in time"} );
+    }
+  });
+
 // update <- db change   /recipe       //edit.ejs & index.ejs
 router.put("/:id/foodItem", async (req, res) => {
     // res.send('UPDATE IS READ')
@@ -109,29 +123,60 @@ router.put("/:id/foodItem", async (req, res) => {
     
   });
 
-
-// // PUT  (update) PANTRY FOOD ITEM
-// router.put("/:id", async (req, res) => {
-//     try {
-//       const updatedItem = {
-//         $push: {pantry: {
-//                 foodItem: req.body.foodItem,
-//                 quantity: req.body.quantity,
-//                 unit: req.body.unit,
-//         }},
-//       };
-//       const updatedPantry = await db.User.findByIdAndUpdate(req.params.id, updatedItem, { new: true });
-//       res.redirect(`/users/${req.params.id}`)
-//     } catch (error) {
-//       console.log(error);
-//       res.send( {message: "Something went horribly wrong [in your PUT Pantry route] please go back... in time"} );
-//     }
-//   }); 
+// GET recipe_id/foodtiem_id --- SAME FUNCTION AS PANTRY FOOD ITEM DIFF ROUTE BUT NEEDS RECIPE ID ---
+router.get("/:recipe_id/:id/editItem", loginRequired, (req, res) => {
+    db.Recipe.findById(req.params.recipe_id,  (err, foundRecipe) => {
+      if (err) {
+      console.log(error)
+      return res.send(err);
+      }
+      // this grabs the recipe & fooditems associates with recipe
+      const context = { recipe : foundRecipe, item: foundRecipe.foodItems.id(req.params.id) };
+      res.render("recipe/editItem", context );
+    });
+    // res.send("fooditem ping back!")
+  }); 
 
 
+// PUT recipe_id/foodtiem_id
+router.put("/:recipe_id/:id/updateItem", loginRequired, async (req, res) => {
+    try {
+      // get the recipe from db
+      const foundRecipe = await db.Recipe.findById(req.params.recipe_id)
+      // get the recipe.foodItem{foodites,qty,unit}
+      let item = foundRecipe.foodItems.id(req.params.id)
+      // update items from db (left) to items from form (right)
+      item.foodName = req.body.foodName;
+      item.quantity = req.body.quantity;
+      item.unit = req.body.unit;
+      // just await then save the user
+      await foundRecipe.save();
+      // construct context to send back to page
+      const context = { recipe: foundRecipe, user: req.session.currentUser }
+      res.render(`recipe/show`, context);
+    } catch (error) {
+      console.log(error);
+      res.send( {message: "Something went horribly wrong [in your PUT recipe.fooditem route] please go back... in time"} );
+    }
+});
 
-
-
+// DELETE recipe_id/foodtiem_id
+router.delete("/:recipe_id/:id/updateItem", loginRequired, async (req, res) => {
+    try {
+      // get the recipe from db
+      const foundRecipe = await db.Recipe.findById(req.params.recipe_id)
+      // get the recipe.foodItems{foodites,qty,unit} and just dot remove & then save
+      let item = foundRecipe.foodItems.id(req.params.id);
+      item.remove();
+      await foundRecipe.save();
+      // construct user context to send back to page
+      const context = { recipe: foundRecipe, user: req.session.currentUser  }
+      res.render(`recipe/show`, context);
+    } catch (error) {
+      console.log(error);
+      res.send( {message: "Something went horribly wrong [in your DELETE recipe FOOD Item route] please go back... in time"} );
+    }
+  });
 
 // delete   N/A
 router.delete("/:id", (req,res) => {
